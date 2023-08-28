eee50572db1811d677029a5d4f2a1bbfa59142d3
KenjaminButton <kennethpchang@gmail.com>
2023-08-29 02:46:32 +0800

    use context using the useContext hook

--- contextapp/src/components/BookList.jsx
+++ contextapp/src/components/BookList.jsx
@@ -1,33 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-  
-  render() {
-    
-    
-    return (
-      <ThemeContext.Consumer>{(context) => {
+// class BookList extends Component {
+//   render() {
+//     return (
+//       <ThemeContext.Consumer>{(context) => {
+//         const {isLightTheme, light, dark} = context
+//         const theme = isLightTheme ? light : dark
+//         return (
+//           <div className='book-list' style={{color: theme.syntax, background: theme.bg}}>
+//             <ul>
+//               <li style={{background: theme.ui}}>The Way of Kings</li>
+//               <li style={{background: theme.ui}}>The Adventures of Tom Sawyer</li>
+//               <li style={{background: theme.ui}}>The Adventures of Huckleberry Finn</li>
+//               <li style={{background: theme.ui}}>The Prince and the Pauper</li>
+//             </ul>
+//           </div>
+//         )
+//       }}
+//       </ThemeContext.Consumer>
+//     )
+//   }
+// }
 
-        const {isLightTheme, light, dark} = context
-        const theme = isLightTheme ? light : dark
+const BookList = () => {
+  const {isLightTheme, light, dark} = useContext(ThemeContext)
+  const theme = isLightTheme ? light : dark
 
-        return (
-          <div className='book-list' style={{color: theme.syntax, background: theme.bg}}>
-            <ul>
-              <li style={{background: theme.ui}}>The Way of Kings</li>
-              <li style={{background: theme.ui}}>The Adventures of Tom Sawyer</li>
-              <li style={{background: theme.ui}}>The Adventures of Huckleberry Finn</li>
-              <li style={{background: theme.ui}}>The Prince and the Pauper</li>
-            </ul>
-          </div>
-        )
-      }}
-      
-      </ThemeContext.Consumer>
-    )
-  }
+  return (
+    <div className='book-list' style={{color: theme.syntax, background: theme.bg}}>
+      <ul>
+        <li style={{background: theme.ui}}>The Way of Kings</li>
+        <li style={{background: theme.ui}}>The Adventures of Tom Sawyer</li>
+        <li style={{background: theme.ui}}>The Adventures of Huckleberry Finn</li>
+        <li style={{background: theme.ui}}>The Prince and the Pauper</li>
+      </ul>
+    </div>
+  )
 }
-
 
 export default BookList;
