3093fe7cee7ac371d9e9fd23355648d1c5253d0c
KenjaminButton <kennethpchang@gmail.com>
2023-08-28 16:05:19 +0800

    Context API refactored using Consumer method (more flexible with Functional Based Components

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -2,20 +2,29 @@ import React, { Component } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
-  static contextType = ThemeContext
+  
   render() {
-    const {isLightTheme, light, dark} = this.context
-    const theme = isLightTheme ? light : dark
+    
+    
     return (
-      <div className='book-list' style={{color: theme.syntax, background: theme.bg}}>
-        <ul>
-          <li style={{background: theme.ui}}>The Way of Kings</li>
-          <li style={{background: theme.ui}}>The Adventures of Tom Sawyer</li>
-          <li style={{background: theme.ui}}>The Adventures of Huckleberry Finn</li>
-          <li style={{background: theme.ui}}>The Prince and the Pauper</li>
-        </ul>
+      <ThemeContext.Consumer>{(context) => {
 
-      </div>
+        const {isLightTheme, light, dark} = context
+        const theme = isLightTheme ? light : dark
+
+        return (
+          <div className='book-list' style={{color: theme.syntax, background: theme.bg}}>
+            <ul>
+              <li style={{background: theme.ui}}>The Way of Kings</li>
+              <li style={{background: theme.ui}}>The Adventures of Tom Sawyer</li>
+              <li style={{background: theme.ui}}>The Adventures of Huckleberry Finn</li>
+              <li style={{background: theme.ui}}>The Prince and the Pauper</li>
+            </ul>
+          </div>
+        )
+      }}
+      
+      </ThemeContext.Consumer>
     )
   }
 }
