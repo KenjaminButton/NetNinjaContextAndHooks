92bd18c27727bfad8cfaea6ea434b10e9175de80
KenjaminButton <kennethpchang@gmail.com>
2023-08-31 17:07:10 +0800

    BookContext refactored to useReducer. BookForm and BookDetails refactored to use dispatch. App working flawlessly

--- booklist/src/contexts/BookContext.jsx
+++ booklist/src/contexts/BookContext.jsx
@@ -1,26 +1,31 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useReducer } from "react"
+import { bookReducer } from "../reducers/bookReducer"
 
 
 
 export const BookContext = createContext()
 
 const BookContextProvider = (props) => {
-  const [books, setBooks] = useState([
-    {title: 'The Adventures of Huckleberry Finn', author: 'Mark Twain', id: 1},
-    {title: 'The Adventures of Tom Sawyer', author: 'Mark Twain', id: 2},
-  ])
-  // Add a new book to data
-  const addBook = (title, author) => {
-    setBooks([...books, {title, author, id: uuidv4() }])
-  }
+  // const [books, setBooks] = useState([
+  //   {title: 'The Adventures of Huckleberry Finn', author: 'Mark Twain', id: 1},
+  //   {title: 'The Adventures of Tom Sawyer', author: 'Mark Twain', id: 2},
+  // ])
 
-  // Remove a book from data
-  const removeBook = (id) => {
-    setBooks(books.filter(book => book.id !== id))
-  }
+  const [books, dispatch] = useReducer(bookReducer, [])
+
+  // // Add a new book to data
+  // const addBook = (title, author) => {
+  //   setBooks([...books, {title, author, id: uuidv4() }])
+  // }
+
+  // // Remove a book from data
+  // const removeBook = (id) => {
+  //   setBooks(books.filter(book => book.id !== id))
+  // }
 
   return (
-    <BookContext.Provider value={{books, addBook, removeBook}}>
+    // <BookContext.Provider value={{books, addBook, removeBook}}>
+    <BookContext.Provider value={{books, dispatch}}>
       {props.children}
     </BookContext.Provider>
   )
